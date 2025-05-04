2ecd22f9db4cb4798c4f801d49ef10d3ecbe4470
coolport <aidanalcayde@gmail.com>
2025-05-04 19:13:54 +0800

    init

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 // import { connectDB } from './config/db.js';
-// import router from "./routes/product.route.js";
+import router from "./routes/product.route.js";
 import cors from "cors";
 
 const PORT = process.env.PORT;
@@ -11,17 +11,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+//pwede naman din ilagay actual routing dito
+//but better if separated yung conerns like in this case
+//where we have a routes dir and file
 app.use("/", router)
 
 app.get('/test', (req, res) => {
   try {
-    res.send("Serving /test");
   } catch {
+    res.send("Serving /test. Server is up.");
     console.error("Failed with error:", error.message);
   };
 })
 
 app.listen(PORT, () => {
-  console.log("Server is running on PORT:", tempport);
+  console.log("Server is running on PORT:", PORT);
 });
 
