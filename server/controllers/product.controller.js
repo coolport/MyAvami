0d6feddbda71e64322043fa0ddb8b079a799921d
coolport <aidanalcayde@gmail.com>
2025-05-04 19:13:38 +0800

    refactor old model for myavami

---
+++ server/controllers/product.controller.js
@@ -0,0 +1,90 @@
+import Product from "../models/product.model.js";
+import mongoose from 'mongoose';
+
+
+
+export const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({}); //returns all products, see docs for things u can {query}
+    res.status(200).json({ success: true, data: products, string: "hi" });
+    // res.status(200).json({ data: products });  trying this for json parse unexpected char at line 1 error in client
+
+
+  } catch (error) {
+    console.error("Error fetching products: ", error.message);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+}
+
+
+export const postProducts = async (req, res) => {
+  const product = req.body; //Return Value: Object 
+  // The req.body property is used to access the data sent by the client in POST requests. 
+
+  if (!product.title || !product.message || !product.image) {
+    return res.status(400).json({ success: false, message: "Please provide all fields" })
+  }
+
+  const newProduct = new Product(product)
+  //create new instance of Product model using data received from the product request.
+  //aka a document. an instandce of a model is a document. aka record. see model file docs
+
+  try {
+    await newProduct.save();
+    console.log('REQPARAMS: ', req.params);
+    res.status(201).json({ success: true, data: newProduct });
+
+  } catch (error) {
+    console.error("Error in Create Product: ", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+    //you can chain this because .status() method returns a response object.
+    //Status Code: 500
+    //Headers: Content-Type: application/json
+    //Body: '{ "success": false, "message": "Server Error" }'
+  }
+
+}
+
+
+//patch - update some fields
+//put - update "all" fields both pretty specific use cases tho
+//put and patch both can be used in this simple case, i tested and its fine
+export const putProduct = async (req, res) => {
+  const { id } = req.params;
+  const product = req.body;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ success: false, message: "Invalid Product Id" });
+  }
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, { new: true }); //check new docs/tooltip (mongoose query prop)
+    res.status(200).json({ success: true, data: updatedProduct });
+
+  } catch (error) {
+    console.error("Error in PUTTING Product: ", error.message);
+    res.status(500).json({ success: false, message: "Server Error" });
+  }
+}
+
+
+export const deleteProduct = async (req, res) => {
+  const { id } = req.params; //get "dynamic" id via deconstruction
+  console.log(req.params);
+  console.log("id:", id);
+
+  try {
+    // await Product.findById(id);
+    await Product.findByIdAndDelete(id)
+    console.log('REQPARAMS: ', req.params);
+    res.status(200).json({ success: true, message: "Product Deleted" });
+
+    // /products/theIDbelow  
+    // id: 67d5a039f9908df397e708c5
+    // REQPARAMS:  { id: '67d5a039f9908df397e708c5' }
+
+  } catch (error) {
+    console.error("Error: ", error.message);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+}
---
+++ server/models/product.model.js
@@ -0,0 +1,45 @@
+import mongoose from 'mongoose';
+
+//Create a product schema
+//schema defines struct of documents (records) in mdb collections
+//defines fields(attrib) of object and what data type they are
+const productSchema = new mongoose.Schema(
+  { //Fields definition (1st arg in schema func)
+    productName: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    },
+    count: {
+      type: Number,
+      required: true
+    },
+    image: {
+      type: String,
+      required: true
+    },
+  }, { //Options Object (2nd argument in schema func)
+  timestamps: true // createdAt, updatedAt (mongoose)
+});
+// Example document (record): https://i.imgur.com/UyJqVSM.png
+
+//Create model
+//from docs: Models are fancy constructors compiled from Schema definitions. An instance of a model is called a document. 
+//Models are responsible for creating and reading documents from the underlying MongoDB database.
+const Product = mongoose.model('Product', productSchema); //(CollectionName, CreatedSchema)
+//'Product' (products as converted by mongoose) represents a mongodb collection
+//each document inside this collection follows the mentioned schema
+
+// now this is possible because we applied model constructor
+// const newProduct = new Product({
+//   name: 'Smartphone',
+//   price: 799,
+//   image: 'smartphone.jpg'
+// });
+// await newProduct.save(); // Saves to MongoDB
+
+export default Product;
+
