933989f39408ebfecc051e3792532f8938b70988
coolport <aidanalcayde@gmail.com>
2025-05-04 19:13:59 +0800

    set routes

---
+++ server/routes/product.route.js
@@ -0,0 +1,14 @@
+//Presentational layer (routes in this case)
+//this is where logic is passed from the controller
+import express from 'express';
+import { getProducts, postProducts, deleteProduct, putProduct } from '../controllers/product.controller.js';
+
+const router = express.Router();
+
+//Create routes
+router.get("/", getProducts);
+router.post("/", postProducts);
+router.put("/:id", putProduct);
+router.delete("/:id", deleteProduct);
+
+export default router;
