3bb8f2ecef09924e71041ce50960b4661e705e95
coolport <aidanalcayde@gmail.com>
2025-05-05 20:48:24 +0800

    define user schema

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,1 +1,24 @@
-// WIP
+// WIP - modeling documentation is in product model
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    role: {
+      type: String,
+      required: true
+    },
+  }, {
+  timestamps: true
+});
+
+const User = mongoose.model('User', userSchema);
+export default User;
+
