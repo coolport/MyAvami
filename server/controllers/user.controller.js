c7eda28e5d8e97007514c241aae63e678dfe4abb
coolport <aidanalcayde@gmail.com>
2025-05-27 22:09:37 +0800

    docs

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -4,13 +4,13 @@ import mongoose from 'mongoose';
 
 export const getUser = async (req, res) => {
   try {
-    const users = await User.find({}); //returns all products, see docs for things u can {query}
+    const users = await User.find({}); //returns all users, see docs for things u can {query}
     res.status(200).json({ success: true, data: users, string: "hi" });
     // res.status(200).json({ data: products });  trying this for json parse unexpected char at line 1 error in client
 
 
   } catch (error) {
-    console.error("Error fetching products: ", error.message);
+    console.error("Error fetching users: ", error.message);
     res.status(500).json({ success: false, message: "Server error" });
   }
 }
@@ -35,7 +35,7 @@ export const postUser = async (req, res) => {
   }
 
   const newProduct = new User(user)
-  //create new instance of Product model using data received from the product request.
+  //create new instance of User model using data received from the product request.
   //aka a document. an instandce of a model is a document. aka record. see model file docs
 
   try {
