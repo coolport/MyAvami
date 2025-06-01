f5fa2f09f9b2d174477959e0d122c8b4a4e82b03
coolport <aidanalcayde@gmail.com>
2025-06-02 07:01:30 +0800

    refactor: productRouter

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -3,7 +3,7 @@
 import express from 'express';
 import { getProducts, postProducts, deleteProduct, putProduct } from '../controllers/product.controller.js';
 
-const router = express.Router();
+const productRouter = express.Router();
 
 //Create routes
 router.get("/", getProducts);
@@ -11,4 +11,4 @@ router.post("/", postProducts);
 router.put("/:id", putProduct);
 router.delete("/:id", deleteProduct);
 
-export default router;
+export default productRouter;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
-import router from "./routes/product.route.js";
+import productRouter from "./routes/product.route.js";
 import userRouter from "./routes/user.route.js";
 import cors from "cors";
 
@@ -33,13 +33,13 @@ app.use((req, res, next) => {
 //pwede naman din ilagay actual routing dito
 //but better if separated yung conerns like in this case
 //where we have a routes dir and file
-app.use("/products", router)
+app.use("/products", productRouter)
 app.use("/users", userRouter)
 
 app.get('/test', (req, res) => {
   try {
-  } catch {
     res.send("Serving /test. Server is up.");
+  } catch (error) {
     console.error("Failed with error:", error.message);
   };
 })
