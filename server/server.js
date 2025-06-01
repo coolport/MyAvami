240da54ae45dfc5070493099866ae6ef28e16aba
coolport <aidanalcayde@gmail.com>
2025-06-02 05:49:46 +0800

    feat: start logging middleware

--- server/server.js
+++ server/server.js
@@ -13,6 +13,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  // TODO: move to middlewares as proper logger. +needs more info below etc
+  // Random logging middleware to log timestamp on every request. 
+  // without mount ('/endpoint', (reqresnext)) aka an endpoint, middlewares
+  // just run everytime a request is made on the whole serv regardless of endpoint. 
+  const reqEndpoint = req.originalUrl;
+  const reqMethod = req.method;
+  console.log();
+
+  const datenow = Date.now();
+  // New Request: 1748780063871
+  const date = new Date(datenow)
+  // New Request:  2025-06-01T12:15:18.405Z
+  console.log(`New Request (${reqMethod}) to ${reqEndpoint}.`, date.toString());
+  next();
+});
+
 //pwede naman din ilagay actual routing dito
 //but better if separated yung conerns like in this case
 //where we have a routes dir and file
