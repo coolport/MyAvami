4383bab0faf24abf35a5693615cb671931a5a07e
coolport <aidanalcayde@gmail.com>
2025-06-03 22:11:09 +0800

    chore: transaction routing

---
+++ server/routes/transaction.route.js
@@ -0,0 +1,12 @@
+import express from 'express';
+// import { getProducts, postProducts, deleteProduct, putProduct } from '../controllers/product.controller.js';
+
+const transactionRouter = express.Router();
+
+//Create routes
+transactionRouter.get("/", getTransactions);
+transactionRouter.post("/", postTransactions);
+// transactionRouter.put("/:id", );
+// transactionRouter.delete("/:id", );
+
+export default transactionRouter;
