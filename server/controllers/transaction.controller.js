5562ccf762c11cd356703c3c2d025792bfeaf579
coolport <aidanalcayde@gmail.com>
2025-06-03 22:11:01 +0800

    feat: transaction controllers

---
+++ server/controllers/transaction.controller.js
@@ -0,0 +1,29 @@
+import Transaction from "../models/transaction.model";
+import mongoose from 'mongoose';
+
+const url = 
+
+export const getTransactions = async (req, res) => {
+  try {
+    const transactions = await Transaction.find({});
+    res.status(200).json({ success: true, data: transactions, string: "works" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ success: false, message: "failedzz" });
+  }
+}
+
+export const postTransactions = async (req, res) => {
+  const transaction = req.body;
+  const newTransaction = new Transaction(transaction);
+
+  try {
+    await newTransaction.save();
+    console.log('REQPARAMS: ', transaction);
+    res.status(201).json({ success: true, data: newTransaction });
+  } catch (error) {
+    console.error(console.error);
+    res.status(200).json({ success: false, message: "failed" });
+  }
+}
+
