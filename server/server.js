7292dffee16126a8b322fee1772f4946d997365f
coolport <aidanalcayde@gmail.com>
2025-06-03 22:11:29 +0800

    feat: mount transaction router

--- server/server.js
+++ server/server.js
@@ -34,6 +34,7 @@ app.use((req, res, next) => {
 // signature is like ("endpoint", middleware/router)
 app.use("/products", productRouter)
 app.use("/users", userRouter)
+app.use("/transaction", transactionRouter)
 
 app.get('/test', (req, res) => {
   try {
