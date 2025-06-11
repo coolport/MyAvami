355b3354ae310da7818c944ac0754e32b471c7c3
coolport <aidanalcayde@gmail.com>
2025-06-12 01:46:33 +0800

    feat: render notifications

--- client/src/Notifications.jsx
+++ client/src/Notifications.jsx
@@ -1,9 +1,57 @@
-
+import { HStack, Card, Button, Box, Stack, Image, Float, Table } from "@chakra-ui/react"
+import { useEffect, useState } from "react"
 
 function Notifications() {
+
+  const [notifications, setNotifications] = useState([]);
+
+  async function getNotifications() {
+    const url = "http://localhost:5555/notifications";
+    try {
+      const response = await fetch(url);
+      const json = await response.json();
+      const notificationsJSON = json.data;
+      // console.log(notificationsJSON);
+      // console.log(notificationsJSON[1])
+      const updatedArray = [];
+
+      for (const x in notificationsJSON) {
+        // console.log(notificationsJSON[x]);
+        updatedArray.push(notificationsJSON[x]);
+      }
+      console.log("Updated Array: ", updatedArray);
+      setNotifications(updatedArray);
+    } catch (error) {
+      console.error("Server error:", error.message);
+    }
+  }
+
+  useEffect(() => {
+    getNotifications();
+    console.log("getNotifications() Triggered")
+  }, [])
+
   return (
     <>
-      Notifications Page
+      {notifications.map((notification, index) => (
+        <Box width="50%" bgColor={"yellow"} key={index}>
+          {/* color of container lol not visible bc of border radius 0 */}
+          <Card.Root borderRadius={"0"}>
+            <Stack>
+              <Card.Header>{notification.notificationTitle}</Card.Header>
+              {/* <Card.Header>PHP {item.itemPrice}</Card.Header> */}
+              <Card.Body>
+                body
+                {/* <Image rounded="md" src={item.itemImage} alt={item.itemName} /> */}
+              </Card.Body>
+              <Card.Footer>
+                {/* <Button onClick={() => handleDelete(item)}>Delete</Button> */}
+                hi
+              </Card.Footer>
+            </Stack>
+          </Card.Root>
+        </Box>
+      ))}
     </>
   )
 }
