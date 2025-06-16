43dec079b01f61c142662e33b9029b04605ece39
coolport <aidanalcayde@gmail.com>
2025-06-16 11:18:52 +0800

    .

--- client/src/Login.jsx
+++ client/src/Login.jsx
@@ -1,36 +1,35 @@
-import { Box, Button, Center, Stack } from "@chakra-ui/react"
-import { useForm } from "react-hook-form"
-
+import { Box, Button, Center, Stack, Text } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
 
 function Login() {
-
   const { register, handleSubmit } = useForm();
 
   async function onSubmit(data) {
-    // const response = await
     console.log(data);
   }
 
   return (
     <>
       <Center>
-        <Box width={"50%"} marginTop={50}>
+        <Box width={"50%"} marginTop={50} color={"black"}>
+          temp login
           <form onSubmit={handleSubmit(onSubmit)}>
-            <Stack>
-              username <input {...register("username",
-                { required: true })}
-              />
-              password <input {...register("password",
-                { required: true, })}
-              />
+            <Stack spacing={4}>
+              <Text color="gray.700">Username</Text>
+              <input {...register("username", { required: true })} />
+
+              <Text color="gray.700">Password</Text>
+              <input {...register("password", { required: true })} />
+
+              <Button type="submit" mt={4} colorScheme="blue">
+                Login
+              </Button>
             </Stack>
-            <Button type="submit">Submit</Button>
-          </form >
+          </form>
         </Box>
       </Center>
     </>
-  )
+  );
 }
 
-
-export default Login
+export default Login;
