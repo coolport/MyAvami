a4099d95d46f76885a430d4659c89aead03db521
coolport <aidanalcayde@gmail.com>
2025-06-16 11:19:10 +0800

    fix: validation, etc

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -6,7 +6,7 @@ export const getUser = async (req, res) => {
   try {
     const users = await User.find({}); //returns all users, see docs for things u can {query}
     res.status(200).json({ success: true, data: users, string: "hi" });
-    // res.status(200).json({ data: products });  trying this for json parse unexpected char at line 1 error in client
+    // res.status(200).json({ data: user });  trying this for json parse unexpected char at line 1 error in client
 
 
   } catch (error) {
@@ -16,32 +16,21 @@ export const getUser = async (req, res) => {
 }
 
 export const postUser = async (req, res) => {
-  // try {
-  //
-  //   const reqBody = req.body;
-  //
-  //   console.log(reqBody)
-  //
-  // } catch {
-  //   res.status(400).json({ success: false, message: "Server error." });
-  //   console.error("Error:", error.message)
-  // }
-
   const user = req.body; //Return Value: Object 
   // The req.body property is used to access the data sent by the client in POST requests. 
 
-  if (!user.username || !user.password || !user.role) {
+  if (!user.userUsername || !user.userPassword || !user.userFullName || !user.userRole) {
     return res.status(400).json({ success: false, message: "Please provide all fields" })
   }
 
-  const newProduct = new User(user)
-  //create new instance of User model using data received from the product request.
+  const newUser = new User(user)
+  //create new instance of User model using data received from the user request.
   //aka a document. an instandce of a model is a document. aka record. see model file docs
 
   try {
-    await newProduct.save();
+    await newUser.save();
     console.log('REQPARAMS: ', req.params);
-    res.status(201).json({ success: true, data: newProduct });
+    res.status(201).json({ success: true, data: newUser });
 
   } catch (error) {
     console.error("Error in Create user: ", error.message);
