2ab4fe5005accd17855aff8b41fb8fb5e1e48f39
coolport <aidanalcayde@gmail.com>
2025-06-18 04:38:49 +0800

    feat: employee homepage

---
+++ client/src/HomeEmployee.jsx
@@ -0,0 +1,43 @@
+
+import React from 'react';
+import { Link as RouterLink } from 'react-router';
+import {
+  FiShoppingCart,
+  FiClock,
+  FiBell,
+} from 'react-icons/fi';
+import DashboardHeader from './components/DashboardHeader';
+import styles from './styles/Home.module.css';
+
+const features = [
+  { label: 'Transact', icon: FiShoppingCart, path: '/sales' },
+  { label: 'Transaction History', icon: FiClock, path: '/transacthistory' },
+  { label: 'Notifications', icon: FiBell, path: '/notifications' },
+];
+
+const HomepageEmployee = () => {
+  return (
+    <>
+      <DashboardHeader />
+      <div className={styles.container}>
+        <div className={styles.grid}>
+          {features.map((feature) => {
+            const IconComponent = feature.icon;
+            return (
+              <RouterLink
+                key={feature.label}
+                to={feature.path}
+                className={styles.featureCard}
+              >
+                <IconComponent className={styles.featureIcon} />
+                <p className={styles.featureLabel}>{feature.label}</p>
+              </RouterLink>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default HomepageEmployee;
