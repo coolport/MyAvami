1dcc2c8f81c1d05c63ad2b3c3e4d9886ace6ab58
coolport <aidanalcayde@gmail.com>
2025-06-17 12:45:57 +0800

    keep old product routing

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -2,11 +2,18 @@
 //this is where logic is passed from the controller
 import express from 'express';
 import { getProducts, postProducts, deleteProduct, putProduct } from '../controllers/product.controller.js';
+import { requireLogin, requireRole } from '../middleware/auth.middleware.js';
 
 const productRouter = express.Router();
 
 //Create routes
+
+
 productRouter.get("/", getProducts);
+// apply auth middleware
+// not working rn, wag muna galaawin
+// productRouter.get("/", requireLogin, requireRole("admin"), getProducts);
+
 productRouter.post("/", postProducts);
 productRouter.put("/:id", putProduct);
 productRouter.delete("/:id", deleteProduct);
