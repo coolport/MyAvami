0059b22d5bba3930c7b41ba49f4f84b91fb7ecd8
coolport <aidanalcayde@gmail.com>
2025-06-20 00:04:44 +0800

    util
    unauth

---
+++ client/src/services/getCurrentUser.js
@@ -0,0 +1,18 @@
+const url = " http://localhost:5555/auth/me"
+export async function fetchUser() {
+  try {
+    const res = await fetch(url, {
+      method: "GET",
+      credentials: "include",
+    });
+
+    if (res.ok) {
+      const data = await res.json();
+      return data.user.role
+    } else {
+      console.error("Failed to fetch user");
+    }
+  } catch (err) {
+    console.error("Error fetching user:", err);
+  }
+};
